ba8e5c4ba65679c0eabca8c72ebda496f7f5bd2b
MDBV <45043200+MDBVCoding@users.noreply.github.com>
2021-03-31 15:32:46 -0400

    Update hacerExamen2.php

--- hacerExamen2.php
+++ hacerExamen2.php
@@ -1,5 +1,9 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
 include_once 'db_con.php';
 
 
@@ -34,10 +38,10 @@ if (isset($_POST['cedula']) && isset($_POST['color']) && isset($_POST['aspecto']
 		if ($numresultado > 0) {
 
 			$sql = "INSERT INTO examen2 (cedula, tipoexamen, color, aspecto, olor, consistencia, reaccion, moco, sangre, restosalimentos, sangreoculta, azucaresreductores, ph, vermesadulto, huevos, protozoarios) VALUES ('".$Cedula."', '".$TipoExamen."', '".$color."', '".$aspecto."', '".$olor."', '".$consistencia."', '".$reaccion."', '".$moco."', '".$sangre."', '".$restosalimentos."', '".$sangreoculta."', '".$azucaresreductores."', '".$ph."', '".$vermesadulto."', '".$huevos."', '".$protozoarios."') ";
-				$result = $conn->query($sql) or die($conn->error);
+				$result = $conn->query($sql) or die("ocurrio un error verifique los datos");
 
 			$sqldos = "UPDATE solicitudexamen INNER JOIN examen2 ON solicitudexamen.pacientecedula=examen2.cedula AND solicitudexamen.idtipoexamen=examen2.tipoexamen SET solicitudexamen.estado='".$completado."'";
-			$resultdos = $conn->query($sqldos) or die($conn->error);
+			$resultdos = $conn->query($sqldos) or die("Ocurrio un error");
 
 
 			include_once 'fpdf/fpdf.php';
@@ -75,7 +79,7 @@ if (isset($_POST['cedula']) && isset($_POST['color']) && isset($_POST['aspecto']
 			}
 
 			$pdfquery = "SELECT paciente.Nombre, paciente.Apellido ,paciente.Cedula, paciente.Correo,examen2.Color,examen2.Aspecto,examen2.Olor,examen2.Reaccion,examen2.Ph,examen2.Consistencia,examen2.Moco,examen2.Restosalimentos,examen2.Sangre,examen2.Sangreoculta,examen2.Azucaresreductores,examen2.Vermesadulto,examen2.Huevos,examen2.Protozoarios FROM examen2, solicitudexamen, paciente WHERE examen2.Cedula=solicitudexamen.PacienteCedula AND paciente.Cedula=solicitudexamen.PacienteCedula AND examen2.Cedula='$Cedula'";
-			$pdfresult = $conn->query($pdfquery) or die($conn->error);
+			$pdfresult = $conn->query($pdfquery) or die("ocurrio un error");
 
 			$pdf = new PDF();
 			$pdf->AliasNbPages();
@@ -116,11 +120,45 @@ if (isset($_POST['cedula']) && isset($_POST['color']) && isset($_POST['aspecto']
 				$pdf->Cell(40,10,$row['Huevos'],0,1,'C');
 				$pdf->Cell(40,10,'Protozoarios:',0,0,'C');
 				$pdf->Cell(40,10,$row['Protozoarios'],0,0,'C');
+				
+				$correito = $row['Correo'];
 
 			}
 
 			$pdf->Output('F', 'examen2'.$Cedula.'.pdf');
-			echo "Procesado Correctamente";	
+			
+			require $_SERVER['DOCUMENT_ROOT'] . '/mail/Exception.php';
+            require $_SERVER['DOCUMENT_ROOT'] . '/mail/PHPMailer.php';
+            require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
+
+            $mail = new PHPMailer;
+            $mail->isSMTP(); 
+            $mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+            $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+            $mail->Port = 587; // TLS only
+            $mail->SMTPSecure = 'tls'; // ssl is deprecated
+            $mail->SMTPAuth = true;
+            $mail->Username = 'moisesdbvproyectophp@gmail.com'; // email
+            $mail->Password = ''; // password
+            $mail->setFrom('moisesdbvproyectophp@gmail.com', 'MoiLab'); // From email and name
+            $mail->addAddress($correito, 'Paciente'); // to email and name
+            $mail->Subject = 'Resultados de su Examen de Heces';
+            $mail->msgHTML("Resultados"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+            $mail->AltBody = 'HTML messaging not supported'; // If html emails is not supported by the receiver, show this body
+            // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+            $mail->AddAttachment("examen2".$Cedula.".pdf");
+            $mail->SMTPOptions = array(
+                        'ssl' => array(
+                        '   verify_peer' => false,
+                            'verify_peer_name' => false,
+                            'allow_self_signed' => true
+                        )
+                    );
+            if(!$mail->send()){
+                echo "Mailer Error: " . $mail->ErrorInfo;
+            }else{
+                echo "Procesado Correctamente";
+            }
 		}else {
 			echo "<script>alert('Verifique que el paciente tenga una solicitud pendiente')</script>";
 		}	
